bea3e846984832a60b097f7fadc5248e851be9e2
MikeGatt90 <mikegatt76@gmail.com>
2022-10-11 14:44:22 +0000

    string examples

---
+++ string.py
@@ -0,0 +1,20 @@
+some_string = "I'm learning Python incongention with other technologies to get into the tech sector"
+print(some_string)
+
+first_name = "Michael"
+last_name = "Gatt"
+print("My first name is {}. My last name is {}".format(first_name, last_name))
+
+print(first_name + " " + last_name)
+
+my_int = 100
+sentence = "The total comes to "
+print(sentence + str(my_int))
+
+user = {"first_name":"Mya"}
+print(user)
+{'first_name':'Mya'}
+
+supercars = ["lamborghini", "ferrari", "porsche"]
+print(supercars)
+
