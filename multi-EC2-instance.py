062b5163057d877aff84559806b9489f3b4c0b92
MikeGatt90 <mikegatt76@gmail.com>
2022-10-12 17:27:34 +0000

    Python Script for creating multiple EC2 instances simultaneously

---
+++ multi-EC2-instance.py
@@ -0,0 +1,11 @@
+import boto3
+ec2 = boto3.resource('ec2')
+
+instances = ec2.create_instances(
+        ImageId="ami-026b57f3c383c2eec",
+        MinCount=2,
+        MaxCount=4,
+        InstanceType="t2.micro",
+        KeyName="Python",
+        SubnetId="subnet-072c908573b68f831")
+    
